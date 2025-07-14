b9fbe480182c04fcd2cf3be3b6fb022da9bc03dc
Aitsam Ahad <aitsam@oktogrid.io>
2025-07-14 14:59:31 +0500

    Add landing page for fitProX1

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,28 +1,28 @@
-import React, { lazy } from "react";
-import { Navigate } from "react-router-dom";
-import Loadable from "../components/loadable/Loadable";
+import React, { lazy } from 'react'
+import { Navigate } from 'react-router-dom'
+import Loadable from '../components/loadable/Loadable'
 
-const Layout = Loadable(lazy(() => import("../components/layout/Layout")));
-const Dashboard = Loadable(lazy(() => import("../views/dashboard/Dashboard")));
-const Sell = Loadable(lazy(() => import("../views/sell/Sell")));
-const Login = Loadable(lazy(() => import("../views/login/Login")));
-const Notfound = Loadable(
-  lazy(() => import("../components/errorboundary/404"))
-);
+const Layout = Loadable(lazy(() => import('../components/layout/Layout')))
+const Dashboard = Loadable(lazy(() => import('../views/dashboard/Dashboard')))
+const Landing = Loadable(lazy(() => import('../views/landing')))
+const Sell = Loadable(lazy(() => import('../views/sell/Sell')))
+const Login = Loadable(lazy(() => import('../views/login/Login')))
+const Notfound = Loadable(lazy(() => import('../components/errorboundary/404')))
 
 const Router = [
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      { path: "/", element: <Navigate to="/dashboard" /> },
-      { path: "/dashboard", exact: true, element: <Dashboard /> },
-      { path: "/sell", exact: true, element: <Sell /> },
-      { path: "/login", exact: true, element: <Login /> },
-      { path: "/auth/404", exact: true, element: <Notfound /> },
-      { path: "*", element: <Navigate to="/auth/404" /> }
-    ]
-  }
-];
+	{
+		path: '/',
+		element: <Layout />,
+		children: [
+			{ path: '/', element: <Navigate to="/dashboard" /> },
+			{ path: '/dashboard', exact: true, element: <Dashboard /> },
+			{ path: '/landing', exact: true, element: <Landing /> },
+			{ path: '/sell', exact: true, element: <Sell /> },
+			{ path: '/login', exact: true, element: <Login /> },
+			{ path: '/auth/404', exact: true, element: <Notfound /> },
+			{ path: '*', element: <Navigate to="/auth/404" /> },
+		],
+	},
+]
 
-export default Router;
+export default Router
